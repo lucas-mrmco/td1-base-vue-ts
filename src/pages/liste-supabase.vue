6878936e7b99a0439e8f66ab79b7fac3febb6a8e
lucas-mrmco <lucas.moramarco@edu.univ-fcomte.fr>
2022-09-23 15:07:01 +0200

    affichage de maison valide

--- src/pages/liste-supabase.vue
+++ src/pages/liste-supabase.vue
@@ -1,8 +1,13 @@
 <script setup lang="ts">
 import { supabase } from "../supabase";
 console.log("supabase :", supabase); // pour vérifier et "garder" supabase dans le code
-const { data: maisons, error } = await supabase.from("Maison").select("*");
-console.log("maisons",maisons)
+
+// const { data: maisons, error } = await supabase.from("Maison").select("*");
+// console.log("maisons",maisons)
+
+let { data: maisons, error } = await supabase
+  .from('Maison')
+  .select('*')
 </script>
 
 <template>
